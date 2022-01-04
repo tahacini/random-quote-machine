26c4cfc44d2bf5923ddbe2a742d6f5ecee8f7bad
Taha Cini <taha.cini@outlook.com>
2022-01-04 09:54:51 +0300

    App file created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import QuoteContainer from "./components/QuoteContainer";
+import SocialContainer from "./components/SocialContainer";
 
-function App() {
+const App = () => {
+  const [len, setLen] = useState();
+  const [state, setState] = useState(() => Math.floor(Math.random() * 30));
+  const [datas, setDatas] = useState({});
+  const quote = datas.quote;
+  const author = datas.author;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      fetch(
+        "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setLen(data.quotes.length);
+          setDatas(data.quotes[state]);
+        });
+    }, 250);
+    return () => clearTimeout(timeout);
+  }, [state]);
+
+  function getRandom() {
+    setState(Math.floor(Math.random() * len));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box" className="quote-box">
+      <QuoteContainer quo={quote} aut={author} />
+      <SocialContainer getNum={getRandom} quo={quote} aut={author} />
     </div>
   );
-}
+};
 
 export default App;
